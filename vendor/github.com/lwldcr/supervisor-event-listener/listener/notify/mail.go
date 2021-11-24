6e11f3aee50ddc43a0dfd2a742e5f43af65271db
Bruce Lee <lwldcr@gmail.com>
2021-11-24 14:23:58 +0800

    use go mod

---
+++ vendor/github.com/lwldcr/supervisor-event-listener/listener/notify/mail.go
@@ -0,0 +1,37 @@
+package notify
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/go-gomail/gomail"
+	"github.com/lwldcr/supervisor-event-listener/event"
+)
+
+type Mail struct{}
+
+func (mail *Mail) Send(message event.Message) error {
+	body := message.String()
+	body = strings.Replace(body, "\n", "<br>", -1)
+	gomailMessage := gomail.NewMessage()
+	gomailMessage.SetHeader("From", Conf.MailServer.User)
+	gomailMessage.SetHeader("To", Conf.MailUser.Email...)
+	gomailMessage.SetHeader("Subject", "Supervisor事件通知")
+	gomailMessage.SetBody("text/html", body)
+
+	mailer := gomail.NewDialer(
+		Conf.MailServer.Host,
+		Conf.MailServer.Port,
+		Conf.MailServer.User,
+		Conf.MailServer.Password,
+	)
+		
+	err := mailer.DialAndSend(gomailMessage)
+	if err == nil {
+		return nil
+	}
+	errorMessage := fmt.Sprintf("邮件发送失败#%s", err.Error())
+
+	return errors.New(errorMessage)
+}
